824d74d40c01143dd6a7c8627d31f05b5434c7a4
otabakoglu <otabakoglu@gmail.com>
2017-04-26 00:45:55 +0300

    t

---
+++ lib/src/main/java/com/example/Fax.java
@@ -0,0 +1,9 @@
+package com.example;
+
+/**
+ * Created by Rubi on 24.04.2017.
+ */
+
+public interface Fax {
+    void fax();
+}
---
+++ lib/src/main/java/com/example/MultiPrinter.java
@@ -0,0 +1,8 @@
+package com.example;
+
+/**
+ * Created by Rubi on 24.04.2017.
+ */
+
+public class MultiPrinter extends Printer {
+}
